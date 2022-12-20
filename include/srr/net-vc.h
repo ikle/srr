1ab4b1a534828dc803290ed6255b3678112f267c
Alexei A. Smekalkine <ikle@ikle.ru>
2022-12-20 22:37:41 +0300

    Add network virtual circuit interface

---
+++ include/srr/net-vc.h
@@ -0,0 +1,38 @@
+/*
+ * SRR Net Virtual Circuits
+ *
+ * Copyright (c) 2022 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef SRR_NET_VC_H
+#define SRR_NET_VC_H  1
+
+#include <stddef.h>
+
+struct net_vc_info {
+	int nid, pid, len, flags;
+};
+
+/*
+ * The virtual circuit process is a message proxy process (forked from
+ * the net process) reparented and reowned to the caller process.
+ *
+ * The function net_vc_attach establishes a connection to the remote
+ * process pid on node nid. Returns the pid of the gateway process.
+ * Requires execute access to the net process.
+ *
+ * The function net_vc_detach closes the connection to the remote
+ * process. Requires write access to the virtual circuit process.
+ *
+ * The function net_vc_connect looks up a remote process by name and
+ * establishes a connection to it. Returns the pid of the gateway
+ * process. Requires execute access to the net process.
+ */
+int net_vc_attach  (int nid, int pid, int len, int flags);
+int net_vc_detach  (int pid);
+int net_vc_connect (int nid, const char *name, int len, int flags);
+int net_vc_query   (int vid, struct net_vc_info *vi);
+
+#endif  /* SRR_NET_VC_H */
