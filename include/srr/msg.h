d69d966a446d27fa765d43943e036bb5d650df26
Alexei A. Smekalkine <ikle@ikle.ru>
2022-12-20 22:36:05 +0300

    Add basic kernel message interface

---
+++ include/srr/msg.h
@@ -0,0 +1,37 @@
+/*
+ * SRR Kernel: Messages
+ *
+ * Copyright (c) 2022 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef SRR_MSG_H
+#define SRR_MSG_H  1
+
+#include <stddef.h>
+
+/*
+ * The function msg_send sends a request to the process.
+ *
+ * The function msg_receive accepts a request from clients and returns
+ * the process identifier of the caller and size of request and answer
+ * buffers.
+ *
+ * The function msg_reply sends an answer to the process for the request
+ * previously accepted by msg_receive.
+ *
+ * The function msg_read reads an extra request data from client request
+ * (between msg_receive and msg_reply).
+ *
+ * The function msg_write writes an extra answer data to client (between
+ * msg_receive and msg_reply)
+ */
+int msg_send (int pid, const void *req, size_t rlen, void *ans, size_t alen);
+int msg_receive (int pid, void *req, size_t *rlen, size_t *alen);
+
+int msg_read  (int pid, size_t offset, void *req, size_t len);
+int msg_write (int pid, size_t offset, const void *ans, size_t len);
+int msg_reply (int pid, int ret, const void *ans, size_t alen);
+
+#endif  /* SRR_MSG_H */
