b46b5c976a5a7324fa279112f7a85246530bed53
Alexei A. Smekalkine <ikle@ikle.ru>
2022-12-21 19:10:58 +0300

    proc-control: redesign and clean up interface

--- include/srr/proc-control.h
+++ include/srr/proc-control.h
@@ -10,6 +10,7 @@
 #define SRR_PROC_CONTROL_H  1
 
 #include <stddef.h>
+#include <stdint.h>
 
 /*
  * Functions that change the value of process parameters always return
@@ -17,6 +18,8 @@
  * want to read the current value.
  */
 enum proc_op {
+	PROC_TARGET	= 0,		/* select target process	*/
+
 	PROC_SCHED	= 1,		/* scheduler algorithm		*/
 	PROC_PRIO	= 2,		/* process priority		*/
 
@@ -39,13 +42,13 @@ enum proc_op {
 };
 
 /*
- * The function proc_control executes a zero-terminated sequence of simple
- * commands atomically: if at least one command cannot be executed, then
- * no command at all will be executed.
+ * The function proc_control executes a sequence of count simple single
+ * operand commands atomically: if at least one command cannot be
+ * executed, then no command at all will be executed.
  *
  * On success returns a copy of the request with the arguments replaced
  * by the results.
  */
-int proc_control (int proc, int pid, const int *req, int *ans, size_t size);
+int proc_control (int proc, const int32_t *req, int32_t *ans, size_t count);
 
 #endif  /* SRR_PROC_CONTROL_H */
