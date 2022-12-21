c6a66ad103eb7acefa7712e786b6c785f72a5e5c
Alexei A. Smekalkine <ikle@ikle.ru>
2022-12-21 21:05:07 +0300

    proc-clock: clean up interface

--- include/srr/proc-clock.h
+++ include/srr/proc-clock.h
@@ -13,19 +13,18 @@
 
 /*
  * The function proc_clock_gettime retrieve the time of the specified
- * clock. Requires read access to the proc process.
+ * clock.
  *
  * The function proc_clock_settime set the time of the specified clock.
- * Requires write access to the proc process.
  *
  * The function proc_clock_adjtime adjusts the specified clock by delta
  * nanoseconds if delta is not zero. The clock is partially adjusted
  * every system tick by (tick-size)/rate until it has been adjusted by
  * the entire delta amount. If the left is not NULL, then the remaining
- * delta is stored in it. Requires write access to the proc process.
+ * delta is stored in it.
  */
-int proc_clock_gettime (int pid, int clock, struct timespec *ts);
-int proc_clock_settime (int pid, int clock, const struct timespec *ts);
-int proc_clock_adjtime (int pid, int clock, long delta, int rate, long *left);
+int proc_clock_gettime (int proc, int clock, struct timespec *ts);
+int proc_clock_settime (int proc, int clock, const struct timespec *ts);
+int proc_clock_adjtime (int proc, int clock, long delta, int rate, long *left);
 
 #endif  /* SRR_PROC_CLOCK_H */
