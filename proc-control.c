21ee34bce1c6884d6180d03a1aac9c8d538eadb6
Alexei A. Smekalkine <ikle@ikle.ru>
2022-12-22 20:20:20 +0300

    proc-control: remove proc aliasing

--- proc-control.c
+++ proc-control.c
@@ -13,7 +13,5 @@ int proc_control (int proc, const int32_t *req, int32_t *ans, size_t count)
 {
 	const size_t len = sizeof (req[0]) * 2 * count;
 
-	proc = proc == 0 ? PROC_PID : proc;
-
 	return msg_send (proc, req, len, ans, len);
 }
