57339ab7a3e46be27a3ddc02cd0a034bf539b469
Alexei A. Smekalkine <ikle@ikle.ru>
2022-12-26 10:27:07 +0300

    wire/proc: reserve codes for proc-spawn

--- include/srr/wire/proc.h
+++ include/srr/wire/proc.h
@@ -14,6 +14,9 @@
 #define PROC_PID  1
 
 enum proc_code {
+	PROC_SPAWN		= 0x00,  // elf-loader copied from proc space to new process...
+	PROC_SPAWN_DONE,
+
 	PROC_SEGMENT_ALLOC	= 0x10,
 	PROC_SEGMENT_FREE,
 	PROC_SEGMENT_QUERY,
